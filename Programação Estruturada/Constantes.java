66f0128699e37dd43b0b715403e44a9abe548139
Damaris Késia Vaz de Oliveira <30970124+Damariskesia@users.noreply.github.com>
2020-05-20 19:17:23 -0300

    Add files via upload

---
+++ Programação Estruturada/Constantes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantes;
+
+/**
+ *
+ * @author Irês
+ */
+public class Constantes {
+
+   public static final int constante = 10;
+   public static int variavel= 10;
+   
+    public static void main(String[] args) {
+        System.out.println("constante " + constante);
+        System.out.println("variavel " + variavel);
+        //constante=20
+        variavel=20;
+        System.out.println("constante " + constante);
+        System.out.println("variavel " + variavel);
+    }
+    
+}
