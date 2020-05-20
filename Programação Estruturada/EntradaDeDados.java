ad5036e1aa6a008cac2cb67945c751e024b66dbf
Damaris Késia Vaz de Oliveira <30970124+Damariskesia@users.noreply.github.com>
2020-05-20 20:17:59 -0300

    Add files via upload

---
+++ Programação Estruturada/EntradaDeDados.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Irês
+ */
+import java.util.Scanner;
+
+public class EntradaDeDados {
+    public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite seu nome:");
+		String nome = leitor.nextLine();
+		System.out.println("Olá, " + nome);
+	}
+}
